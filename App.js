d4c6c85f72d7fad030f2669571bf8afacb8ba209
Education <education@Cristians-MBP.lan>
2020-11-15 19:17:56 +0100

    Agregafo el StyleSheet

--- App.js
+++ App.js
@@ -1,36 +1,53 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
   View,
   Text,
   Image,
   TextInput,
+  StyleSheet
 } from 'react-native';
 
 const App = () => {
   return (
-      <SafeAreaView>
-        <ScrollView>
-          <View>
-            <Text>Estocolmo</Text>
-            <Text>Nublado</Text>
-            <Text>10°</Text>
-            <Image 
-              source={{uri: 'https://s3-us-west-2.amazonaws.com/melingoimages/Images/17425.jpg'}}
-              style={{width: 200, height: 200}}
-             />
-          </View>
-          <TextInput
-            style={{
-              height: 40,
-              borderColor: 'gray',
-              borderWidth: 1
-            }}
-          />
-        </ScrollView>
-      </SafeAreaView>
+    <View style={styles.contenedor}>
+      <Text style={styles.textoLargo}>Estocolmo</Text>
+      <Text style={styles.textoNormal}>Nublado</Text>
+      <Text style={styles.textoMedio}>10°</Text>
+      <Image 
+        source={{uri: 'https://s3-us-west-2.amazonaws.com/melingoimages/Images/17425.jpg'}}
+        style={{width: 200, height: 200}}
+      />
+      <TextInput
+        style={styles.textInput}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  contenedor: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 20
+  },
+  textoLargo: {
+    fontSize: 48
+  },
+  textoMedio: {
+    fontSize: 42
+  },
+  textoNormal: {
+    fontSize: 18
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 300,
+    marginTop: 20,
+    paddingHorizontal: 10
+  }
+})
 
 export default App;
