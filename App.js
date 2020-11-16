7ca170c7a54a88bdf31a321f3d0596646d48bc26
Education <education@Cristians-MBP.lan>
2020-11-16 08:22:53 +0100

    Fetch de datos de un API

--- App.js
+++ App.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   SafeAreaView,
   View,
   Text,
   Image,
   TextInput,
-  StyleSheet
+  StyleSheet,
+  ScrollView,
 } from 'react-native';
+import axios from 'axios';
 
 const App = () => {
+  const [ciudad, actualizarCiudad] = useState({
+    title: '',
+    location_type: '',
+    woeid: 0,
+    latt_long: ''
+  });
+  const [query, setQuery] = useState('stockholm');
+  useEffect(() => {
+    async function fetchClima() {
+      const resp = await axios(
+        'https://www.metaweather.com/api/location/search/?query=stockholm',
+      );
+ 
+      actualizarCiudad(resp.data);
+    }
+
+    fetchClima();
+  }, []);
+  console.log(query)
+
   return (
     <SafeAreaView
       style={styles.contenedor}
     >
-      <View style={styles.contenedor}>
-        <Text style={styles.textoLargo}>Estocolmo</Text>
-        <Text style={styles.textoNormal}>Nublado</Text>
-        <Text style={styles.textoMedio}>10°</Text>
-        <Image 
-          source={{uri: 'https://s3-us-west-2.amazonaws.com/melingoimages/Images/17425.jpg'}}
-          style={{width: 200, height: 200}}
-        />
-        <TextInput
-          style={styles.textInput}
-        />
+      <View style={styles.view}>
+          <Text style={styles.textoLargo}>Estocolmo</Text>
+          <Text style={styles.textoNormal}>Nublado</Text>
+          <Text style={styles.textoMedio}>10°</Text>
+          <Image 
+            source={{uri: 'https://cdn4.iconfinder.com/data/icons/weather-forecast-flat-1/64/rain_rainy_weather_weather_forecast-512.png'}}
+            style={{width: 200, height: 200}}
+          />
+          <TextInput
+            style={styles.textInput}
+            onChangeText={(e) => setQuery(e)}
+          />
       </View>
     </SafeAreaView>
   );
@@ -31,6 +54,9 @@ const App = () => {
 
 const styles = StyleSheet.create({
   contenedor: {
+    flex: 1,
+  },
+  view: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
