416058d5d700e7a56e6615474fea59406ec2d437
Education <education@Cristians-MBP.lan>
2020-11-16 08:29:33 +0100

    Mas mejoras en estilos

--- App.js
+++ App.js
@@ -1,36 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   View,
   Text,
   Image,
   TextInput,
-  StyleSheet,
-  ScrollView,
+  StyleSheet
 } from 'react-native';
-import axios from 'axios';
 
 const App = () => {
-  const [ciudad, actualizarCiudad] = useState({
-    title: '',
-    location_type: '',
-    woeid: 0,
-    latt_long: ''
-  });
-  const [query, setQuery] = useState('stockholm');
-  useEffect(() => {
-    async function fetchClima() {
-      const resp = await axios(
-        'https://www.metaweather.com/api/location/search/?query=stockholm',
-      );
- 
-      actualizarCiudad(resp.data);
-    }
-
-    fetchClima();
-  }, []);
-  console.log(query)
-
   return (
     <SafeAreaView
       style={styles.contenedor}
@@ -41,11 +19,10 @@ const App = () => {
         <Text style={styles.textoMedio}>10°</Text>
         <Image 
           source={{uri: 'https://cdn4.iconfinder.com/data/icons/weather-forecast-flat-1/64/rain_rainy_weather_weather_forecast-512.png'}}
-          style={{width: 200, height: 200}}
+          style={styles.imagen}
         />
         <TextInput
           style={styles.textInput}
-          onChangeText={(e) => setQuery(e)}
         />
       </View>
     </SafeAreaView>
@@ -70,6 +47,10 @@ const styles = StyleSheet.create({
   },
   textoNormal: {
     fontSize: 18
+  },
+  imagen: {
+    width: 200, 
+    height: 200
   },
   textInput: {
     height: 40,
